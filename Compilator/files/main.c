dc4d7d5329ab991133326dcc9080ce65e06aab0c
Turcuș Adrian <adrian.turcus@student.upt.ro>
2024-04-27 00:18:03 +0300

    Added Lexer
    Lexical analysis

---
+++ Compilator/files/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+#include "lexer.h"
+
+int main(){
+  char *inbuf=loadFile("tests/testlex.c");
+  puts(inbuf);
+  Token *tokens=tokenize(inbuf);
+  showTokens(tokens);
+  free(inbuf);
+  return 0;
+}
